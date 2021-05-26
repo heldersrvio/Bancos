4d758a74b942a655fe92c16d371ca978b6389691
Helder Sérvio <heldersrvio@gmail.com>
2021-05-26 20:35:09 -0300

    Added handling of code inexistence to bankname route

--- back-end/main.py
+++ back-end/main.py
@@ -2,7 +2,7 @@ from app import app
 from db import mysql
 from flask import Flask, request
 
-@app.route('/bankslist')
+@app.route('/bankslist', methods=['GET'])
 def banksList():
     cursor = mysql.get_db().cursor()
     query = 'SELECT NOME FROM BANCOS'
@@ -13,13 +13,13 @@ def banksList():
         'list': nomes
     }
 
-@app.route('/bankname/<bank_code>')
+@app.route('/bankname/<bank_code>', methods=['GET'])
 def bankCode(bank_code):
     cursor = mysql.get_db().cursor()
     print(bank_code)
     query = 'SELECT NOME FROM BANCOS WHERE CODIGO = %s'
     cursor.execute(query, bank_code)
-    nome = cursor.fetchall()[0][0]
+    nome = None if len(cursor.fetchall()) == 0 else cursor.fetchall()[0][0]
     cursor.close()
     return {
         'name': nome
