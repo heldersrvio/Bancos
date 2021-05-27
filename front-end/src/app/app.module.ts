c2143e04dc6430c2bc08ecc2e17ddb99a5c35184
Helder Sérvio <heldersrvio@gmail.com>
2021-05-26 21:54:50 -0300

    Added top bar to front end and changed initializeDatabase.py to use mysql import from db.py

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
