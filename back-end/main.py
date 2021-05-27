81db6ee9c384829b6f4523cdfbad17426082ed7e
Helder Sérvio <heldersrvio@gmail.com>
2021-05-26 22:58:45 -0300

    Revert "Changed main.py to access database directly"
    This reverts commit 25908baac594837f29cb9350165d44649bde83b1.

--- back-end/main.py
+++ back-end/main.py
@@ -1,13 +1,12 @@
 from app import app
-import mysql.connector
 import pymysql
 from db import mysql
 from flask import Flask, request
 
 @app.route('/bankslist', methods=['GET'])
 def banksList():
-    connection = mysql.connector.connect(host=os.environ.get('DB_HOST'), database=os.environ.get('DB_DB'), user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASSWORD'))
-    cursor = connection.cursor()
+    connection = mysql.connect()
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
     query = 'SELECT NOME FROM BANCOS'
     cursor.execute(query)
     nomes = [row[0] for row in cursor.fetchall()]
@@ -18,8 +17,8 @@ def banksList():
 
 @app.route('/bankname/<bank_code>', methods=['GET'])
 def bankCode(bank_code):
-    connection = mysql.connector.connect(host=os.environ.get('DB_HOST'), database=os.environ.get('DB_DB'), user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASSWORD'))
-    cursor = connection.cursor()
+    connection = mysql.connect()
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
     print(bank_code)
     query = 'SELECT NOME FROM BANCOS WHERE CODIGO = %s'
     cursor.execute(query, bank_code)
