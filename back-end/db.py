1acb6827a26c40d82710602d4af1778a5c5eaa94
Helder Sérvio <heldersrvio@gmail.com>
2021-05-26 23:07:13 -0300

    Removed dotenv from db.py

--- back-end/db.py
+++ back-end/db.py
@@ -1,13 +1,11 @@
 import os
-from dotenv import load_dotenv
 from app import app
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
-load_dotenv()
 
-app.config['MYSQL_DATABASE_USER'] = os.environ.get('DB_USER')
-app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get('DB_PASSWORD')
-app.config['MYSQL_DATABASE_DB'] = os.environ.get('DB_DB')
-app.config['MYSQL_DATABASE_HOST'] = os.environ.get('DB_HOST')
+app.config['MYSQL_DATABASE_USER'] = os.environ['DB_USER']
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['DB_PASSWORD']
+app.config['MYSQL_DATABASE_DB'] = os.environ['DB_DB']
+app.config['MYSQL_DATABASE_HOST'] = os.environ['DB_HOST']
 mysql.init_app(app)
