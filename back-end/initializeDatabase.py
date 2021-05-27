992f02c173f168ffe2d0d923690cf5d967de9184
Helder Sérvio <heldersrvio@gmail.com>
2021-05-26 22:07:52 -0300

    Created BANCOS table

--- back-end/initializeDatabase.py
+++ back-end/initializeDatabase.py
@@ -1,18 +1,28 @@
-from db import mysql
+import os
+import mysql.connector
 import xlrd
+from dotenv import load_dotenv
+from mysql.connector import Error
+
+load_dotenv()
 
 try:
-	table = xlrd.open_workbook('bancos.xls')
-	sheet1 = table.sheet_by_index(0)
-	cursor = mysql.get_db().cursor()
-	cursor.execute('CREATE TABLE IF NOT EXISTS BANCOS (CODIGO INT NOT NULL, NOME VARCHAR(100), PRIMARY KEY (CODIGO))')
-	for row in range(1, sheet1.nrows):
-		query = 'INSERT INTO BANCOS (CODIGO, NOME) VALUES (%s, %s)'
-		record = (sheet1.cell_value(rowx=row, colx=0), sheet1.cell_value(rowx=row, colx=1))
-		result = cursor.execute(query, record)
-		print('({codigo}, {nome}) inserted into table'.format(codigo=sheet1.cell_value(rowx=row, colx=0), nome=sheet1.cell_value(rowx=row, colx=1)))
-except Exception as error:
+	connection = mysql.connector.connect(host=os.environ.get('DB_HOST'), database=os.environ.get('DB_DB'), user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASSWORD'))
+	if connection.is_connected():
+		table = xlrd.open_workbook('bancos.xls')
+		sheet1 = table.sheet_by_index(0)
+		cursor = connection.cursor()
+		cursor.execute('CREATE TABLE IF NOT EXISTS BANCOS (CODIGO INT NOT NULL, NOME VARCHAR(100), PRIMARY KEY (CODIGO))')
+		for row in range(1, sheet1.nrows):
+			query = 'INSERT INTO BANCOS (CODIGO, NOME) VALUES (%s, %s)'
+			record = (sheet1.cell_value(rowx=row, colx=0), sheet1.cell_value(rowx=row, colx=1))
+			result = cursor.execute(query, record)
+			connection.commit()
+			print('({codigo}, {nome}) inserted into table'.format(codigo=sheet1.cell_value(rowx=row, colx=0), nome=sheet1.cell_value(rowx=row, colx=1)))
+except mysql.connector.Error as error:
 	print('Failed to create table in MySQL: {}'.format(error))
 finally:
-	cursor.close()
-	print('Cursor is closed')
+	if connection.is_connected():
+		cursor.close()
+		connection.close()
+		print('MySQL connection is closed')
